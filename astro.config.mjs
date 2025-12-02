a233978bfcbf0184c1d0521e243cf7cad1ad5ae3
winkel-titouan <titouan.winkel@edu.univ-fcomte.fr>
2025-12-03 00:07:32 +0100

    actualisation avec pocketbase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   }
